f55d3e21c5765c1d8b59e5ad27b427de85fda305
Qihan Wang <henrybalek@gmail.com>
2024-05-10 14:15:36 -0400

    feat:end of all function,no update from now

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const hourData=new Date();
-let hours=hourData.getHours();
-console.log(hours)
+const hourData = new Date();
+// let li=document.getElementsByTagName('li').;
+let years = document.getElementById("years");
+let month = document.getElementById("month");
+let days = document.getElementById("days");
+let hours = document.getElementById("hours");
+let mintues = document.getElementById("mintues");
+let second = document.getElementById("sceonds");
+let timeZone = document.getElementsByTagName("h2");
+// console.log(timeZone)
+timeZone[0].innerHTML += Intl.DateTimeFormat().resolvedOptions().timeZone;
+years.innerHTML = "Today is year " + hourData.getFullYear();
+month.innerHTML = hourData.getMonth() + 1;
+days.innerHTML = hourData.getDate();
+hours.innerHTML = hourData.getHours();
+mintues.innerHTML = hourData.getMinutes();
+second.innerHTML = hourData.getSeconds();
+// let hours = hourData.getHours();
+setTimeout(function(){
+    window.location.reload(1);
+ }, 1000);
+// console.log(days);
+
+// years.innerHTML = hourData.getFullYear();
