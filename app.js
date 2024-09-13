facc4887868bbdaefc13757a46e35e0bc42b5546
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-09-13 11:53:41 -0400

    wrapped data retrieval and update in function calling function with previous setTimeout block

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const hourData = new Date();
-// let li=document.getElementsByTagName('li').;
-let years = document.getElementById("years");
-let month = document.getElementById("month");
-let days = document.getElementById("days");
-let hours = document.getElementById("hours");
-let mintues = document.getElementById("mintues");
-let second = document.getElementById("sceonds");
-let timeZone = document.getElementsByTagName("h2");
-// console.log(timeZone)
-timeZone[0].innerHTML += Intl.DateTimeFormat().resolvedOptions().timeZone;
-years.innerHTML = "Today is year " + hourData.getFullYear();
-month.innerHTML = hourData.getMonth() + 1;
-days.innerHTML = hourData.getDate();
-hours.innerHTML = hourData.getHours();
-mintues.innerHTML = hourData.getMinutes();
-second.innerHTML = hourData.getSeconds();
-// let hours = hourData.getHours();
-setTimeout(function(){
-    window.location.reload(1);
- }, 1000);
-// console.log(days);
-
-// years.innerHTML = hourData.getFullYear();
+const setTime = () => {
+    const hourData = new Date();
+    
+    // getting the HTML elements
+    let years = document.getElementById("years");
+    let month = document.getElementById("month");
+    let days = document.getElementById("days");
+    let hours = document.getElementById("hours");
+    let mintues = document.getElementById("mintues");
+    let second = document.getElementById("sceonds");
+    let timeZone = document.getElementsByTagName("h2");
+    
+    // setting the time info into the HTML elements
+    timeZone[0].innerHTML = `Current Timezone: ${Intl.DateTimeFormat().resolvedOptions().timeZone}`
+    years.innerHTML = "Today is year " + hourData.getFullYear();
+    month.innerHTML = hourData.getMonth() + 1;
+    days.innerHTML = hourData.getDate();
+    hours.innerHTML = hourData.getHours();
+    mintues.innerHTML = hourData.getMinutes();
+    second.innerHTML = hourData.getSeconds();
+    
+    // Changing the info after one second
+    setTimeout(function(){
+        setTime()
+     }, 1000);
+}
+setTime()
